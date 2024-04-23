7bbec8dfc66c3538bebed35bff4724fe3e26d1c4
vctt <victor.duarte.oliveira@gmail.com>
2024-04-23 16:23:13 -0300

    Add swagger docs builder

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 const validationPipeService = require('@pipets/validation-pipes');
 
 async function bootstrap() {
@@ -8,6 +9,18 @@ async function bootstrap() {
     validationPipeService();
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
+
+    const config = new DocumentBuilder()
+    .setTitle('Cats Adoption API')
+      .setDescription(`Welcome to the Cats Adoption API,
+        a Tundrax Assignment project designed to facilitate the adoption of cats.
+        This API provides endpoints for managing cat profiles and user interactions.`)
+      .addTag('Cats')
+      .addTag('Users')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document);
+
     await app.listen(3000);
     console.log(`Application is running on: ${await app.getUrl()}`);
   } catch(err) {
