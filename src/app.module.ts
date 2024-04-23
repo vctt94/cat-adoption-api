401eceaf07ebc0b7ab0daa323b841fe3ff9da08f
vctt <victor.duarte.oliveira@gmail.com>
2024-04-23 16:22:14 -0300

    Add database configuration, common entities, and modules to application modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from "./database/database.module";
 import { CatsModule } from './cats/cats.module';
 import { CoreModule } from './core/core.module';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CoreModule, CatsModule],
+  imports: [
+    // global configuration module
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    CoreModule,
+    AuthModule,
+    UsersModule,
+    CatsModule,
+  ],
 })
 export class AppModule {}
